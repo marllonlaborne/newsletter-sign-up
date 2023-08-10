7aad2cf6c2dc100fbab2b7d65cbb6735ab575971
Marllon <99225101+marllonlaborne@users.noreply.github.com>
2023-08-10 16:33:25 -0300

    js code improvements

--- src/js/app.js
+++ src/js/app.js
@@ -4,16 +4,21 @@ const newsletter = document.querySelector('.newsletter-container')
 const subscribe = document.querySelector('#form button')
 const postSubscription = document.querySelector('.success-subscription')
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault()
-  
-  subHandleClick(event)
-})
+form.addEventListener('submit', (event) => event.preventDefault())
 
-const getUserEmail = (event) => {
-  const userEmail = event.target.value
+const subHandleClick = (event) => {
+  event.preventDefault()
+
+  // Get the user's email from email input field
+  const userEmail = emailInput.value
+
+  // Check if the user's email is invalid or not
   const isEmailInvalid = userEmail === '' || !checkEmail(userEmail)
+
+  // Get the previous element `span` from `emailInput`
   const previousElement = emailInput.previousElementSibling.lastElementChild
+
+  // Get the paragraph element from the `postSubscripton` div
   const postSubParagraph = postSubscription.querySelector('p')
   
   if (isEmailInvalid) {
@@ -21,22 +26,21 @@ const getUserEmail = (event) => {
     previousElement.classList.add('error-message')
     previousElement.textContent = 'Valid email required'
   } else {
+    emailInput.classList.remove('error-input')
+    previousElement.classList.remove('error-message')
+    previousElement.textContent = ''
     setInterval(() => {
       newsletter.style.display = 'none'
       postSubscription.style.display = 'flex'
       postSubParagraph.textContent = `
-        A confirmation email has been sent to ${userEmail}. Please open it and click the button inside to confirm your subscription.
+        A confirmation email has been sent to ${userEmail}. 
+        Please open it and click the button inside to confirm your subscription.
       `
     }, 1000)
   }
 }
 
-const subHandleClick = (event) => {
-  event.preventDefault()
-
-  getUserEmail(event)
-}
-
+// Function responsible for validating the user's email
 const checkEmail = (email) => {
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
 }
